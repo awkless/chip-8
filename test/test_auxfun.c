d568dffdc9f74b53e68d70a423dc12addbbf8994
Jason Pena <jasonpena@awkless.com>
2023-03-05 20:20:30 -0700

    chore(chip8_readrom()): Test chip8_readrom()
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- test/test_auxfun.c
+++ test/test_auxfun.c
@@ -11,6 +11,24 @@
 #include "utils/auxfun.h"
 
 /*
+ * Default stub rom to test with.
+ */
+#define DEFAULT_ROM_FILE "test/roms/stub.ch8"
+
+/*
+ * Expected binary data from stub rom. These bytes represent the following
+ * CHIP-8 instructions:
+ *
+ * LD V1, 2
+ * LD V2, 3
+ * ADD V1, V2
+ */
+#define EXPECTED_ROM_DATA_SIZE 6
+static const uint8_t EXPECTED_ROM_DATA[EXPECTED_ROM_DATA_SIZE] = {
+	0x61, 0x02, 0x62, 0x03, 0x81, 0x24
+};
+
+/*
  * Test chip8_arrsize() macro.
  *
  * TEST TYPES:
@@ -24,11 +42,46 @@ static void test_chip8_arrsize(void)
 }
 
 /*
+ * Test test_chip8_readrom().
+ *
+ * TEST TYPES:
+ *   1. chip8_readrom() catches NULL file path.
+ *   2. chip8_readrom() catches non-existant file path.
+ *   3. chip8_readrom() catches NULL size parameter.
+ *   4. chip8_readrom() returns no errors for opening a binary file.
+ *   5. chip8_readrom() counts binary file size correctly in bytes.
+ *   6. chip8_readrom() reads binary files correctly.
+ */
+static void test_chip8_readrom(void)
+{
+	char *path = DEFAULT_ROM_FILE;
+	uint8_t *buffer = NULL;
+	size_t size = 0;
+	chip8_error code = 0;
+	cmp_ok(chip8_readrom(NULL, &buffer, &size), "==", CHIP8_EINVAL,
+	       "chip8_readrom() catches NULL file path");
+	cmp_ok(chip8_readrom("badrom.ch8", &buffer, &size), "==", CHIP8_ENOFILE,
+	       "chip8_readrom() catches non-existant file path");
+	cmp_ok(chip8_readrom(path, &buffer, NULL), "==", CHIP8_EINVAL,
+	       "chip8_readrom() catches NULL buffer size variable");
+
+	code = chip8_readrom(path, &buffer, &size);
+	cmp_ok(code, "==", CHIP8_EOK,
+	       "chip8_readrom() returns no error for opening a binary file");
+	cmp_ok(EXPECTED_ROM_DATA_SIZE, "==", size,
+	       "chip8_readrom() counts binary file size correctly");
+	cmp_mem(buffer, EXPECTED_ROM_DATA, size,
+		"chip8_readrom() reads binary files correctly)");
+
+}
+
+/*
  * Starting point of test suite.
  */
 int main(void)
 {
 	plan(7);
 	test_chip8_arrsize();
+	test_chip8_readrom();
 	done_testing();
 }
