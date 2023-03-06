30a8fe5a102540337a31a8fe555107e4a38f4c22
Jason Pena <jasonpena@awkless.com>
2023-03-05 19:18:08 -0700

    feat(utils/error.c): Define error code messages and chip8_die()
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/utils/error.c
@@ -0,0 +1,28 @@
+/* SPDX-FileCopyrightText: 2023 Jason Pena <jasonpena@awkless.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils/error.h"
+
+static const char *const ERROR_MESSAGE[] = {
+	[CHIP8_EOK] = "success",
+	[CHIP8_EINVAL] = "invalid argument",
+	[CHIP8_ENOMEM] = "no memory",
+	[CHIP8_ENOFILE] = "no such file exists",
+	[CHIP8_EBIGFILE] = "file is too big to load"
+};
+
+void chip8_die(chip8_error code)
+{
+	fprintf(stderr, "chip-8: ");
+	if (code >= CHIP8_ECOUNT) {
+		fprintf(stderr, "%s\n", ERROR_MESSAGE[CHIP8_EINVAL]);
+		exit(CHIP8_EINVAL);
+	}
+
+	fprintf(stderr, "%s\n", ERROR_MESSAGE[code]);
+	exit(code);
+}
