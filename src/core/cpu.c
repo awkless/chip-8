d27112eb9af0699bf5c999a9964396fa433726d4
Jason Pena <jasonpena@awkless.com>
2023-03-07 16:29:06 -0700

    feat(core/cpu.c): Define chip8_cpu_cycle()
    Currently the foundation is provided for instruction decoding, but the
    actual instructions themselves must be implemented.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/core/cpu.c
+++ src/core/cpu.c
@@ -103,3 +103,93 @@ chip8_error chip8_cpu_reset(chip8_cpu **cpu)
 
 	return CHIP8_EOK;
 }
+
+/*
+ * CHIP-8 uses 16-bit fixed width instructions that can come in the following
+ * three formats:
+ *
+ *   1. CXYN
+ *   2. CXNN
+ *   3. CNNN
+ *
+ * Where C is the code or group, X and Y refer to general purpose registers V0
+ * through VF, and N represents a nibble (4-bit) constant (so N is 4-bits, NN
+ * is 8-bits, and NNN is 12-bits).
+ *
+ * Thus, instruction decoding is essentially performed per nibble of an opcode.
+ * Hence why four basic 8-bit variables c, xn, yn, and n are used to represent
+ * each nibble bit field of an opcode for this implementation.
+ */
+chip8_error chip8_cpu_cycle(chip8_cpu **cpu)
+{
+	uint16_t opcode = 0; /* Full 16-bit opcode. */
+	uint8_t c = 0;       /* Operation bit field (Cxxx). */
+	uint8_t xn = 0;      /* VX or N bit field (xX/Nxx). */
+	uint8_t yn = 0;      /* VY or N bit field (xxY/Nxx). */
+	uint8_t n = 0;       /* N bit field (xxxN). */
+	chip8_error flag = CHIP8_EOK;
+
+	if (!*cpu)
+		return CHIP8_EINVAL;
+
+	/* Fetch */
+	opcode = (*cpu)->memory[(*cpu)->pc] << 8 | (*cpu)->memory[(*cpu)->pc + 1];
+	c = opcode >> 12;
+	xn = opcode >> 8 & 0xF;
+	yn = opcode >> 4 & 0xF;
+	n = opcode & 0xF;
+	(*cpu)->pc += 2;
+
+	switch (c) {
+	case 0x0:
+		/* TODO */
+		break;
+	case 0x1:
+		/* TODO */
+		break;
+	case 0x2:
+		/* TODO */
+		break;
+	case 0x3:
+		/* TODO */
+		break;
+	case 0x4:
+		/* TODO */
+		break;
+	case 0x5:
+		/* TODO */
+		break;
+	case 0x6:
+		/* TODO */
+		break;
+	case 0x7:
+		/* TODO */
+		break;
+	case 0x8:
+		/* TODO */
+		break;
+	case 0x9:
+		/* TODO */
+		break;
+	case 0xA:
+		/* TODO */
+		break;
+	case 0xB:
+		/* TODO */
+		break;
+	case 0xC:
+		/* TODO */
+		break;
+	case 0xE:
+		/* TODO */
+		break;
+	case 0xF:
+		/* TODO */
+		break;
+	default:
+		flag = CHIP8_EBADOP;
+		break;
+	}
+
+	return flag;
+}
