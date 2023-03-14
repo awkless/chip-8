b411941aee5f6e4c04e6906c842ae0bd8efc5a7a
Jason Pena <jasonpena@awkless.com>
2023-03-14 15:44:33 -0600

    fix(Makefile): All build to fail on test failure
    The `test/run_tests.sh` would not allow the build to fail for failed
    unit test cases. This was problematic given that the CI pipeline catches
    bad unit tests through failed unit test executables.
    
    From now on, we will just name the unit test we want to execute in the
    Makefile directly instead of using an external script.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- test/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# SPDX-FileCopyrightText: 2023 Jason Pena <jasonpena@awkless.com>
-# SPDX-License-Identifier: MIT
-#
-# Simple shell script designed to run all test executables in this directory.
-
-find ./ -type f -name 'test_*' -executable -exec sh -c 'exec "$1"' _ {} \;
