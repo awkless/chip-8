f99f497691e8ced384cf50a561bf147670dff331
Jason Pena <jasonpena@awkless.com>
2023-03-14 18:18:03 -0600

    feat(test/test_keypad.c): Test chip8_keypad_clear()
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- test/test_keypad.c
+++ test/test_keypad.c
@@ -19,11 +19,24 @@ static void test_chip8_keypad_init(void)
 }
 
 /*
+ * Test chip8_keypad_clear()
+ *
+ * TEST TYPES:
+ *   1. chip8_keypad_clear() catches NULL keypad
+ */
+static void test_chip8_keypad_clear(void)
+{
+	cmp_ok(chip8_keypad_clear(NULL), "==", CHIP8_EINVAL,
+	       "chip8_keypad_clear() catches NULL keypad");
+}
+
+/*
  * Starting point of tests.
  */
 int main(void)
 {
-	plan(1);
+	plan(11);
 	test_chip8_keypad_init();
+	test_chip8_keypad_clear();
 	done_testing();
 }
