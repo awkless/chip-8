6ae650f3ec01ec59d33d86f46a2a9e29ccb64437
Jason Pena <jasonpena@awkless.com>
2023-03-27 15:07:26 -0600

    feat(test_video): Test chip8_video_clear()
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- test/test_video.c
+++ test/test_video.c
@@ -34,3 +34,24 @@ static void test_chip8_video_render(void)
 	cmp_ok(chip8_video_render(NULL), "==", CHIP8_EINVAL,
 	       "chip8_video_render() catches NULL argument");
 }
+
+/*
+ * Test chip8_video_clear().
+ *
+ * TEST TYPES
+ *   1. chip8_video_clear() catches NULL argument.
+ */
+static void test_chip8_video_clear(void)
+{
+	cmp_ok(chip8_video_clear(NULL), "==", CHIP8_EINVAL,
+	       "chip8_video_clear() catches NULL argument");
+}
+
+int main(void)
+{
+	plan(3);
+	test_chip8_video_init();
+	test_chip8_video_render();
+	test_chip8_video_clear();
+	done_testing();
+}
