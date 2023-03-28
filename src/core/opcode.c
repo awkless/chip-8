c14db4071803bc1e85552483eae1f104e8eda102
Jason Pena <jasonpena@awkless.com>
2023-03-27 19:35:20 -0600

    feat(core/opcode.c): Define chip8_opcode_00EE()
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/core/opcode.c
+++ src/core/opcode.c
@@ -9,3 +9,10 @@ void chip8_opcode_00E0(chip8_cpu *cpu)
 	chip8_video_clear(cpu->video);
 	chip8_debug("opcode 0E00");
 }
+
+void chip8_opcode_00EE(chip8_cpu *cpu)
+{
+	cpu->sp--;
+	cpu->pc = cpu->stack[cpu->sp];
+	chip8_debug("opcode 00EE");
+}
