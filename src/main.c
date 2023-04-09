83d1dcc521a65427b6bb4ca92c15f8e5fb263217
Jason Pena <jasonpena@awkless.com>
2023-04-09 12:12:25 -0600

    feat(src/main.c): Implement basic CLI using getopt
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "SDL.h"
 #include "utils/error.h"
@@ -11,20 +14,68 @@
 #include "core/video.h"
 #include "core/cpu.h"
 
+static void usage(void)
+{
+	printf("Usage: chip-8 [-l <rom>] [-f <ins/sec>] [-s <scale>] [-v] [-h]\n"
+	       "Simple CHIP-8 emulator.\n\n"
+	       "Options:\n"
+	       "  -l <rom>     CHIP-8 ROM to load and process.\n"
+	       "  -f <ins/sec> CPU speed (instructions per second).\n"
+	       "  -s <scale>   Scale factor for window.\n"
+	       "  -v           Print version info.\n"
+	       "  -h           Print this help info.\n");
+}
+
+static void version(void)
+{
+	printf("CHIP-8 emulator "VERSION"\n\n"
+	       "Written by Jason Pena, Nate Le, and John Cully\n");
+}
+
 /**
  * @brief Starting point of CHIP-8 emulator.
  *
  * @return 0 for success, or some @p chip8_error code to indicate failure.
  */
-int main(void)
+int main(int argc, char **argv)
 {
+	int opt = 0;
+	int freq = 0;
+	int scale = 0;
+	char *rom = NULL;
 	chip8_video *video = NULL;
 	chip8_keypad *keypad = NULL;
 	chip8_cpu *cpu = NULL;
 	chip8_error flag = CHIP8_EOK;
 	bool quit = false;
 
-	flag = chip8_video_init(&video, 10);
+	while ((opt = getopt(argc, argv, "l:f:s:vh")) != -1) {
+		switch (opt) {
+		case 'l':
+			rom = strdup(optarg);
+			break;
+		case 'f':
+			freq = atoi(optarg);
+			break;
+		case 's':
+			scale = atoi(optarg);
+			break;
+		case 'h':
+			usage();
+			exit(EXIT_SUCCESS);
+			break;
+		case 'v':
+			version();
+			exit(EXIT_SUCCESS);
+			break;
+		default:
+			usage();
+			exit(EXIT_FAILURE);
+			break;
+		}
+	}
+
+	flag = chip8_video_init(&video, scale);
 	if (flag != CHIP8_EOK)
 		chip8_die(flag);
 
@@ -32,11 +83,11 @@ int main(void)
 	if (flag != CHIP8_EOK)
 		chip8_die(flag);
 
-	flag = chip8_cpu_init(&cpu, video, keypad, 1);
+	flag = chip8_cpu_init(&cpu, video, keypad, freq);
 	if (flag != CHIP8_EOK)
 		chip8_die(flag);
 
-	flag = chip8_cpu_romload(cpu, "test/roms/ibm_logo.ch8");
+	flag = chip8_cpu_romload(cpu, rom);
 	if (flag != CHIP8_EOK)
 		chip8_die(flag);
 
@@ -51,6 +102,7 @@ int main(void)
 			chip8_die(flag);
 	}
 
+	free(rom);
 	chip8_keypad_free(keypad);
 	chip8_video_free(video);
 	chip8_cpu_free(cpu);
